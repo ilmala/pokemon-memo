e4f6f293816ba29641789cca5a23142b5f85bf1e
luca.malattia <luca.malattia@immobiliare.it>
2022-11-17 09:56:12 +0100

    add game over state

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ const pokemons = [
   { id: 25, name: "Pikachu" },
 ];
 
-const pairOfPokemons = ref(shuffle([...pokemons, ...pokemons]))
+const pairOfPokemons = ref([])
 const picked = ref([])
 const firstPick = ref(null)
 const secondPick = ref(null)
@@ -20,6 +20,15 @@ const founded = ref([])
 
 const score = ref(0)
 const moves = ref(0)
+const over = ref(false)
+
+onMounted(()=>{
+  pairOfPokemons.value = createPairOfPokemon()
+})
+
+const createPairOfPokemon = () => {
+  return shuffle([...pokemons, ...pokemons])
+}
 
 const isFlipped = (index) => {
   const pokemonId = pairOfPokemons.value[index]?.id
@@ -45,6 +54,21 @@ const handleFlip = (index) => {
   }
 
   moves.value += 1;
+
+  if(founded.value.length === 6){
+    setTimeout(()=>{
+      over.value = true;
+    }, 1000)
+  }
+}
+
+const handlePlayAgain = () => {
+  picked.value = []
+  founded.value = []
+  score.value = 0
+  moves.value = 0
+  pairOfPokemons.value = createPairOfPokemon()
+  over.value = false
 }
 
 const resetPickedCard = () => {
@@ -56,7 +80,34 @@ const resetPickedCard = () => {
 </script>
 
 <template>
-  <div class="h-screen flex justify-center items-center">
+<main>
+  <Transition 
+  enter-from-class="opacity-0"
+  enter-active-class="duration-300 ease-out"
+  enter-to-class="opacity-100"
+  leave-from-class="opacity-100"
+  leave-active-class="duration-200 ease-in"
+  leave-to-class="opacity-0"
+  >
+    <div v-if="over" class="fixed z-10 inset-0 overflow-x-hidden overflow-y-auto flex justify-center items-center">
+      <div class="absolute inset-0 bg-zinc-800/30 backdrop-blur-sm"></div>
+      <div class="relative bg-white w-full max-w-sm rounded-xl shadow-md shadow-blue-600/40">
+        <h3 class="p-4 font-bold text-3xl text-blue-800 text-center">Game Over</h3>
+        <div class="p-4 text-center">
+          <p class="text-blue-800">Your score is: {{ score }}</p>
+          <p class="text-blue-800">Maked Moves: {{ moves }}</p>
+
+          <div class="mt-8 flex justify-center">
+            <button @click.prevent="handlePlayAgain" class="px-4 py-2 bg-blue-500 hover:bg-blue-400 font-semibold text-white text-sm rounded-xl shadow-md shadow-blue-600/40 transition duration-150">
+              Play Again
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Transition> 
+
+  <div class="relative h-screen flex justify-center items-center">
     <div class="py-4">
       <h2 class="font-bold text-5xl text-blue-800 text-center">Pokemon Memo</h2>
       
@@ -75,5 +126,5 @@ const resetPickedCard = () => {
       </div>
     </div>
   </div>
-
+</main>
 </template>
